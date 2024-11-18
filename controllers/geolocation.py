c677b056388214df3e2d13cc1815f015ff629b98
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-11-18 19:31:09 +0800

    add type attendance to handle wfa

--- controllers/geolocation.py
+++ controllers/geolocation.py
@@ -19,6 +19,7 @@ class AttendanceAPIController(http.Controller):
             office_lat = json_data.get('office_lat')
             office_long = json_data.get('office_long')
             employee_id = json_data.get('employee_id')
+            type = json_data.get('type')
         except (ValueError, TypeError):
             return request.make_response(
                 json.dumps({
@@ -57,42 +58,52 @@ class AttendanceAPIController(http.Controller):
                 headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
                 status=400
             )
-
-        # start record attendance
-        distance = request.env['digitaliz.attendance'].calculate_distance(latitude, longitude, office_lat, office_long)
-        if distance < 550:
-            employee = request.env['hr.employee'].sudo().browse(employee_id)
-            if employee.exists():
-                geo_ip_response = request.env['digitaliz.attendance'].get_geoip_response(
-                    mode='systray', latitude=latitude, longitude=longitude
-                )
-                employee._attendance_action_change(geo_ip_response)
-                
-                data = request.env['digitaliz.attendance'].get_employee_info_response(employee)
-                return request.make_response(
-                    json.dumps({
-                        'status': 'success',
-                        'message': 'Attendance today recorded',
-                        'data': data
-                    }),
-                    headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
-                    status=200
-                )
-            else:
-                return request.make_response(
-                    json.dumps({
-                        'status': 'error',
-                        'message': 'User is not linked to any employee'
-                    }),
-                    headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
-                    status=404
-                )
-        else:
+        if type is None:
             return request.make_response(
                 json.dumps({
                     'status': 'error',
-                    'message': f'You are too far from the office. Distance: {distance:.2f} meters'
+                    'message': 'Type Attendance is required.'
                 }),
                 headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
                 status=400
             )
+
+        # start record attendance
+        employee = request.env['hr.employee'].sudo().browse(employee_id)
+        if not employee.exists():
+            return request.make_response(
+                json.dumps({
+                    'status': 'error',
+                    'message': 'User is not linked to any employee'
+                }),
+                headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+                status=404
+            )
+            
+        if type == "normal":
+            distance = request.env['digitaliz.attendance'].calculate_distance(latitude, longitude, office_lat, office_long)
+            if distance >= 550:
+                return request.make_response(
+                    json.dumps({
+                        'status': 'error',
+                        'message': f'You are too far from the office. Distance: {distance:.2f} meters'
+                    }),
+                    headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+                    status=400
+                )
+
+        geo_ip_response = request.env['digitaliz.attendance'].get_geoip_response(
+            mode='systray', latitude=latitude, longitude=longitude
+        )
+        employee._attendance_action_change(geo_ip_response)
+        data = request.env['digitaliz.attendance'].get_employee_info_response(employee)
+        
+        return request.make_response(
+            json.dumps({
+                'status': 'success',
+                'message': 'Attendance today recorded',
+                'data': data
+            }),
+            headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+            status=200
+        )
