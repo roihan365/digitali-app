a6f5f5a01d9ed532b0c8dbed8d3de847f98f4119
roihan365 <bitcoiner.banjarmasin@gmail.com>
2024-11-25 14:27:53 +0800

    add endpoint update employee

--- controllers/employee.py
+++ controllers/employee.py
@@ -42,3 +42,59 @@ class ApiEmployeeController(http.Controller):
                 }),
                 headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers()
             )
+
+    @http.route('/api/employee/update', type='http', auth='apikey', methods=['PUT'], csrf=False)
+    def update_employee(self, **kwargs):
+        if request.httprequest.method == 'OPTIONS':
+            return request.make_response(json.dumps({}), headers=CorsHelper.cors_headers())
+        
+        data = request.httprequest.data
+        try:
+            """
+            values: {
+                "name",
+                "work_email",
+                "work_phone",
+                "department_id
+            }"""
+            json_data = json.loads(data)
+            employee_id = json_data.get('employee_id')
+            values = json_data.get('values', {})
+
+            if not employee_id or not values:
+                return request.make_response(
+                    json.dumps({
+                    'status': 'error',
+                    'message': 'Employee ID dan values diperlukan.',
+                }), headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+                    status=400
+                )
+
+            # Cari employee berdasarkan ID
+            employee = request.env['hr.employee'].sudo().browse(employee_id)
+            if not employee.exists():
+                return request.make_response(
+                json.dumps({
+                    'status': 'error',
+                    'message': f'Employee dengan ID {employee_id} tidak ditemukan.',
+                }),
+                    headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+                    status=400
+                )
+
+            # Update employee
+            employee.sudo().write(values)
+            return request.make_response(
+                json.dumps({
+                'status': 'success',
+                'message': f'Employee dengan ID {employee_id} berhasil diupdate.',
+            }), headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers()
+            )
+        except Exception as e:
+            return request.make_response(
+                json.dumps({
+                'status': 'error',
+                'message': f'Error: {str(e)}',
+            }), headers=[('Content-Type', 'application/json')] + CorsHelper.cors_headers(),
+                status=400
+            )
